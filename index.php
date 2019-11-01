70c022dc05e2b6a9631ff1495a17e652e0c4e6ea
PonomareVlad <Git@PonomareVlad.ru>
2019-11-01 19:22:33 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>PonomareVlad</title>
+    <meta content="https://avatars1.githubusercontent.com/u/2877584" property="og:image">
+    <meta content="0;url=http://github.com/ponomareVlad" http-equiv="refresh">
+</head>
+<body>
+</body>
+</html>
