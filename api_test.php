0e680b0cd2561fe9984542509be911a3f446d3aa
PonomareVlad <Git@PonomareVlad.ru>
2019-11-01 20:48:03 +0500

    Test GitHub API

---
+++ api_test.php
@@ -0,0 +1,50 @@
+<?php
+
+function uploadRepoFile($filePath, $fileContent, $message = 'Added file')
+{
+    $request = new HttpRequest();
+    $request->setUrl('https://api.github.com/repos/PonomareVlad/OGLinks/contents/' . $filePath);
+    $request->setMethod(HTTP_METH_PUT);
+
+    $request->setHeaders(array(
+        'cache-control' => 'no-cache',
+        'Connection' => 'keep-alive',
+        'Content-Length' => '152',
+        'Accept-Encoding' => 'gzip, deflate',
+        'Host' => 'api.github.com',
+        'Postman-Token' => '98ee36f1-d4e6-4290-bd44-0643b2c196cd,602cc6b6-c8fa-4295-aec5-fce0d76f2f8b',
+        'Cache-Control' => 'no-cache',
+        'Accept' => '*/*',
+        'User-Agent' => 'PostmanRuntime/7.18.0',
+        'Authorization' => 'Bearer 381e4be5a6091e662250831424993f8d31585006',
+        'Content-Type' => 'application/json'
+    ));
+
+    $request->setBody('{
+  "message": "' . $message . '",
+  "committer": {
+    "name": "PonomareVlad",
+    "email": "git@ponomarevlad.ru"
+  },
+  "content": "' . base64_encode($fileContent) . '"
+}');
+
+    try {
+        $response = $request->send();
+
+        return $response->getBody();
+    } catch (HttpException $ex) {
+        return $ex;
+    }
+}
+
+function createPage($targetUrl, $imgUrl, $title)
+{
+    ob_start();
+    require 'page.php';
+    return ob_get_clean();
+}
+
+if (!isset($_REQUEST['url'], $_REQUEST['image'], $_REQUEST['title'], $_REQUEST['path'])) exit('Error in parameters');
+
+exit(uploadRepoFile($_REQUEST['path'], createPage($_REQUEST['url'], $_REQUEST['image'], $_REQUEST['title']), $_REQUEST['url']));
---
+++ page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title><? $title ?></title>
+    <meta content="<? $imgUrl ?>" property="og:image">
+    <meta content="0;url=<? $targetUrl ?>" http-equiv="refresh">
+</head>
+<body>
+</body>
+</html>
